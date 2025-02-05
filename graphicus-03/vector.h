33744d975ab01561ff9f7acfa98a1840d3760648
Sarah Gosselin <sarah@gosselin.xyz>
2025-02-05 15:49:28 -0500

    wip on vector class

--- graphicus-03/vector.h
+++ graphicus-03/vector.h
@@ -9,16 +9,27 @@
 template<typename T>
 class Vector {
   private:
-    int capacity;
-    int currentMax;
+    int capacity{INIT_VEC_SIZE};
+    int quantity{0};
 
   public:
-    Vector() : capacity(INIT_VEC_SIZE), currentMax{0} {
+    Vector() {
       vectorItem = new T[capacity];
     }
 
+    /*Vector(int cap) : quantity{0} {*/
+    /*  vectorItem = new T[cap];*/
+    /*}*/
+
+
     virtual ~Vector() {
       delete[] vectorItem;
+    }
+
+    Vector(const Vector<T>& copy): capacity(copy.capacity), quantity(copy.quantity) {
+      vectorItem = new T[capacity];
+      for (int i = 0; i < quantity; i++)
+        vectorItem[i] = copy.vectorItem[i];
     }
 
     T operator[] (int index) {
@@ -26,17 +37,17 @@ class Vector {
     }
 
     bool addItem(T &c) {
-      if (currentMax == capacity)
+      if (quantity == capacity)
         increaseSize();
-      vectorItem[currentMax] = c;
-      currentMax++;
+      vectorItem[quantity] = c;
+      quantity++;
       return true;
     }
 
     void increaseSize() {
       capacity *= 2;
       T* newTemp = new T[capacity];
-      for (int i = 0; i < currentMax; i++)
+      for (int i = 0; i < quantity; i++)
         newTemp[i] = vectorItem[i];
 
       delete[] vectorItem;
@@ -46,35 +57,35 @@ class Vector {
     bool emptyVector() {
       delete[] vectorItem;
       capacity = INIT_VEC_SIZE;
-      currentMax = 0;
+      quantity = 0;
       vectorItem = new T[capacity];
       return true;
 
     }
 
     T removeItem(int index) {
-      if (index < 0 || index > currentMax)
+      if (index < 0 || index > quantity)
         return T();
       T item = vectorItem[index];
-      if (index == currentMax) {
+      if (index == quantity) {
         vectorItem[index] = T();
         return item;
       }
-      for (int i = index; i < currentMax; i++)
+      for (int i = index; i < quantity; i++)
         vectorItem[i] = vectorItem[i+1];
-      vectorItem[currentMax] = T();
-      currentMax--;
+      vectorItem[quantity] = T();
+      quantity--;
       return item;
     }
 
     T getItem(int index) {
-    if (index < 0 || index >= currentMax)
+    if (index < 0 || index >= quantity)
       return T();
     return vectorItem[index];
     }
 
     int getCap() { return capacity; }
-    int getCurrentSize() { return currentMax; }
+    int getCurrentSize() { return quantity; }
     T* vectorItem;
 };
 
